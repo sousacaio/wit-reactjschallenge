7b2b8e35cfb1ca2fc6fe52e0da2f445c7046fae5
caio frias <caio.sousa@extreme.digital>
2022-01-30 20:00:03 -0300

    feat: add icons to operations

--- src/Components/Forms.tsx
+++ src/Components/Forms.tsx
@@ -7,6 +7,10 @@ import { Param } from '../Services/Calculator/interfaces';
 import Calculator from '../Services/Calculator/Calculator';
 import { successModal } from '../Helpers/Modals';
 
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ClearIcon from '@mui/icons-material/Clear';
+import PercentIcon from '@mui/icons-material/Percent';
 interface IOperation {
     operationType: 'sum' | 'div' | 'sub' | 'multi'
 }
@@ -14,7 +18,6 @@ interface IOperation {
 export const Forms: React.FC<IOperation> = (props) => {
 
     const { operationType } = props
-
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 
@@ -28,13 +31,34 @@ export const Forms: React.FC<IOperation> = (props) => {
         }
 
         const result = await new Calculator().execute(params, operationType)
-        
+
         await successModal({ text: result.data.body.result.toString(), title: 'Success!' })
     };
 
+    const Icon = () => {
+        switch (operationType) {
+            case 'sum': {
+                return <AddIcon />
+            }
+            case 'div': {
+                return <PercentIcon style={{ transform: 'rotateY(0deg) rotate(45deg)' }} />
+            }
+            case 'sub': {
+                return <RemoveIcon />
+            }
+            case 'multi': {
+                return <ClearIcon />
+            }
+        }
+    }
     return (
         <Container component="main" maxWidth="xl" style={{ display: 'flex', justifyContent: 'center' }} >
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, display: 'flex', flexDirection: 'row', gap: 1 }}>
+            <Box component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1, display: 'flex', flexDirection: 'row', gap: 1, justifyContent: 'space-between' }}
+            >
+
                 <TextField
                     margin="normal"
                     required
@@ -55,7 +79,7 @@ export const Forms: React.FC<IOperation> = (props) => {
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                 >
-                    {operationType}
+                    {Icon()}
                 </Button>
             </Box>
         </Container>
